b7be8dae35428a108cff273351016ece8d19654b
Yulian Prodanov <julprodanov@gmail.com>
2023-04-30 01:26:29 +0300

    add artist endpoints

--- backend/songs/urls.py
+++ backend/songs/urls.py
@@ -1,9 +1,10 @@
 from django.urls import include, path
 from rest_framework import routers
-from songs.views import SongViewSet
+from songs.views import ArtistViewSet, SongViewSet
 
 router = routers.DefaultRouter()
 router.register(r"song", SongViewSet)
+router.register(r"artist", ArtistViewSet)
 
 urlpatterns = [
     path("api/", include(router.urls)),
--- backend/songs/views.py
+++ backend/songs/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
 
-from songs.models import Song
-from songs.serializers import SongSerializer
+from songs.models import Song, Artist
+from songs.serializers import SongSerializer, ArtistSerializer
 
 
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ArtistViewSet(viewsets.ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
     # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
