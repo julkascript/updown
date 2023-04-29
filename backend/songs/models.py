518cca5056f663ba05a8e36175fa1be8872e5c87
Yulian Prodanov <julprodanov@gmail.com>
2023-04-29 23:32:27 +0300

    fix song-artist representations

--- backend/songs/models.py
+++ backend/songs/models.py
@@ -4,19 +4,17 @@ from django.db import models
 class Song(models.Model):
     title = models.CharField(max_length=100, blank=False)
     lyrics = models.JSONField(default=dict)
+    artist = models.ManyToManyField("Artist", related_name="songs")
+    picture_url = models.URLField(blank=True, null=True)
     # album = models.CharField(max_length=100)
     # release_date = models.DateField()
     # genre = models.CharField(max_length=100)
     # likes = models.IntegerField(default=0)
     # dislikes = models.IntegerField(default=0)
 
-    def __str__(self):
-        return self.title + " - " + self.artist
-
 
 class Artist(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    songs = models.ManyToManyField(Song, null=True, blank=True)
 
     def __str__(self):
         return self.name
--- backend/songs/serializers.py
+++ backend/songs/serializers.py
@@ -2,13 +2,18 @@ from rest_framework import serializers
 from .models import Artist, Song
 
 
-class SongSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Song
-        fields = "__all__"
-
-
 class ArtistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Artist
         fields = "__all__"
+
+
+class SongSerializer(serializers.ModelSerializer):
+    artist = ArtistSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = Song
+        fields = ("artist", "title", "lyrics", "id")
+
+    def to_representation(self, instance):
+        return super().to_representation(instance)
